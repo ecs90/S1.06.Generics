d86118ce4981869e3ade1aff2080b800883a1b64
ecs90 <ecs_salas@hotmail.com>
2024-01-05 12:51:19 +0100

    Refactor type Object to String

--- src/n1exercici1/NoGenericMethods.java
+++ src/n1exercici1/NoGenericMethods.java
@@ -1,37 +1,37 @@
 package n1exercici1;
 
 public class NoGenericMethods {
-    private Object obj1;
-    private Object obj2;
-    private Object obj3;
+    private String obj1;
+    private String obj2;
+    private String obj3;
 
-    public NoGenericMethods(Object obj1, Object obj2, Object obj3) {
+    public NoGenericMethods(String obj1, String obj2, String obj3) {
         this.obj1 = obj1;
         this.obj2 = obj2;
         this.obj3 = obj3;
     }
 
-    public Object getObj1() {
+    public String getObj1() {
         return obj1;
     }
 
-    public void setObj1(Object obj1) {
+    public void setObj1(String obj1) {
         this.obj1 = obj1;
     }
 
-    public Object getObj2() {
+    public String getObj2() {
         return obj2;
     }
 
-    public void setObj2(Object obj2) {
+    public void setObj2(String obj2) {
         this.obj2 = obj2;
     }
 
-    public Object getObj3() {
+    public String getObj3() {
         return obj3;
     }
 
-    public void setObj3(Object obj3) {
+    public void setObj3(String obj3) {
         this.obj3 = obj3;
     }
 }
